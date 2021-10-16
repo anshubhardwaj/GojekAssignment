d06092fe351c95fb5c9222ec099d924830dbff6a
anshubhardwaj <anshubhardwaj1993@gmail.com>
2021-10-16 19:34:08 +0530

    SecondCommit

---
+++ app/src/test/java/com/example/gojekassignment/RepositoryUnitTest.kt
@@ -0,0 +1,23 @@
+package com.example.gojekassignment
+
+import android.widget.AutoCompleteTextView
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(JUnit4::class)
+class RepositoryUnitTest {
+    @Test
+    fun whenListisclick(){
+//        val result = arrayOf(AutoCompleteTextView.Validator)
+//        assertThat(result).isEqualTo(true)
+    }
+
+}
